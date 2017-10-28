3f1b3b10bed0de31c0a229c49070868cdeaf2a9b
NOVAR1 <30316002+NOVAR1@users.noreply.github.com>
2017-10-28 04:50:04 +0300

    Update stormcli.sh

--- stormcli.sh
+++ stormcli.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-cd $HOME/STORM
+token_="351460214:AAHEXlncjPyhF22zlKToJa6fAGQlnJU8D6E" 
 
-install() {
-	    cd tg
+function print_logo() {
+   cd tg
 		sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 		sudo apt-get install g++-4.7 -y c++-4.7 -y
 		sudo apt-get update
@@ -35,41 +35,18 @@ function print_logo() {
 	echo -e " "
 	echo -e " "
 	echo -e " "
-	echo -e " "
-}
+	echo
 
-function logo_play() {
-    declare -A txtlogo
-    seconds=" "
-    txtlogo[1]=" "
-    txtlogo[2]=" "
-    txtlogo[3]=" "
-    txtlogo[4]=" "
-    printf "\033[38;5;600m\t"
-    for i in ${!txtlogo[@]}; do
-        for x in `seq 0 ${#txtlogo[$i]}`; do
-            printf "${txtlogo[$i]:$x:1}"
-            sleep $seconds
-        done
-        printf "\n\t"
-    done
-    printf "\n"
-}
+ echo -e "\e[38;5;77m" 
 
-if [ "$1" = "install" ]; then
-  install
-  else
+echo -e "  "
+echo -e "  "
+echo -e "  "
+echo -e "  "
 
-if [ ! -f ./tg/tgcli ]; then
-    echo "tg not found"
-    echo "Run $0 install"
-    exit 1
- fi
+echo -e "  \e[38;5;88m"
 
-
-   print_logo
-   echo -e "\033[38;5;208m"
-   echo -e "
+echo -e "
  __   ___________   ___     _____     __  __
 /  |  |___   ___|  / _ \   | ___ }   |  \/  |
 \_ \      | |     | | | |  | |_) }   | |\/| |
@@ -77,14 +54,112 @@ if [ ! -f ./tg/tgcli ]; then
 |__/      |_|      \___/   |_|  \_\  |_|  |_|
  BY @TAHAJ20 DEV : TAHAJ20            
 "
-   echo -e "    "
-   echo -e "   "
-   echo -e "   "
-   echo -e "     "
-   echo -e "\033[0;00m"
-   echo -e "\e[36m"
-   logo_play
-   #sudo service redis-server restart
-   #./tg/tgcli -s ./bot/bot.lua -l 1 -E $@
-   ./tg/tgcli -s ./bot/bot.lua $@
-fi
+
+echo -e " "
+
+echo -e " "
+
+echo -e " "
+
+echo -e "
+ __   ___________   ___     _____     __  __
+/  |  |___   ___|  / _ \   | ___ }   |  \/  |
+\_ \      | |     | | | |  | |_) }   | |\/| |
+ _) |     | |     | |_| |  |  _< \   | |  | |
+|__/      |_|      \___/   |_|  \_\  |_|  |_|
+ BY @TAHAJ20 DEV : TAHAJ20            
+"
+
+echo -e " "
+
+echo -e " "
+
+echo -e " "
+
+echo -e " "
+
+}
+
+
+if [ ! -f ./tg/tgcli ]; then
+
+echo -e ""
+
+echo -e " "
+
+echo -e " "
+
+echo -e "
+ __   ___________   ___     _____     __  __
+/  |  |___   ___|  / _ \   | ___ }   |  \/  |
+\_ \      | |     | | | |  | |_) }   | |\/| |
+ _) |     | |     | |_| |  |  _< \   | |  | |
+|__/      |_|      \___/   |_|  \_\  |_|  |_|
+ BY @TAHAJ20 DEV : TAHAJ20            
+"
+
+echo -e " "
+
+echo -e "  "
+
+echo -e " "
+
+    echo " "
+
+    echo "Run $0 install"
+
+    exit 1
+
+ fi
+
+if [ ! $token_ ]; then
+
+echo -e ""
+
+echo -e ""
+
+  echo -e "\e[31;1mToken Not found\e"
+
+ exit 1
+
+ fi
+
+
+
+  print_logo
+
+   echo -e ""
+
+echo -e ""
+
+echo -e " "
+
+echo -e "  "
+
+echo -e "  "
+
+echo -e "
+ __   ___________   ___     _____     __  __
+/  |  |___   ___|  / _ \   | ___ }   |  \/  |
+\_ \      | |     | | | |  | |_) }   | |\/| |
+ _) |     | |     | |_| |  |  _< \   | |  | |
+|__/      |_|      \___/   |_|  \_\  |_|  |_|
+ BY @TAHAJ20 DEV : TAHAJ20            
+"
+
+echo -e "  "
+
+echo -e "  "
+
+echo -e "  "
+
+echo -e ""
+
+echo -e " "
+
+echo -e " "
+
+
+curl "https://api.telegram.org/bot"$token_"/sendmessage" -F
+
+./tg/tgcli -s ./bot/bot.lua $@ --bot=$token_

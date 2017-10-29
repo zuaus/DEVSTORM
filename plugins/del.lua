23cec134c46aa52da7b0091c282861c13ae20009
zuaus <30482065+zuaus@users.noreply.github.com>
2017-10-30 02:16:03 +0300

    Add files via upload

---
+++ plugins/del.lua
@@ -0,0 +1,32 @@
+﻿local function delmsg (i,naji)
+    msgs = i.msgs 
+    for k,v in pairs(naji.messages_) do
+        msgs = msgs - 1
+        tdcli.deleteMessages(v.chat_id_,{[0] = v.id_}, dl_cb, cmd)
+        if msgs == 1 then
+            tdcli.deleteMessages(naji.messages_[0].chat_id_,{[0] = naji.messages_[0].id_}, dl_cb, cmd)
+            return false
+        end
+    end
+    tdcli.getChatHistory(naji.messages_[0].chat_id_, naji.messages_[0].id_,0 , 100, delmsg, {msgs=msgs})
+end
+local function run(msg, matches)
+    if matches[1] == 'تنظيف' and is_owner(msg) then
+        if tostring(msg.to.id):match("^-100") then 
+            if tonumber(matches[2]) > 1000 or tonumber(matches[2]) < 1 then
+                return  'يمكنك الحذف من [ 1 الى 100 ] فقط 📝'
+            else
+				tdcli.getChatHistory(msg.to.id, msg.id,0 , 100, delmsg, {msgs=matches[2]})
+				return "الرسائل"..matches[2].."تم تنظيفها 📮"
+            end
+        else
+            return 'تستطيع المسح في المجمواعات السوبر فقط 🔱'
+        end
+    end
+end
+return {
+    patterns = {
+        '^(تنظيف) (%d*)$',
+    },
+    run = run
+}

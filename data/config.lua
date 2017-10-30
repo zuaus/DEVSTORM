e9a857b77d59abb0e5784b1fff7b4de6dd28d674
zuaus <30482065+zuaus@users.noreply.github.com>
2017-10-30 19:15:52 +0300

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,35 @@
+do local _ = {
+  admins = {
+    {
+      430041821,
+      "@q1111p"
+    }
+  },
+  disabled_channels = {},
+  enabled_plugins = {
+    "plog",
+    "del",
+    "mee",
+    "addreply",
+    "banhammer",
+    "help",
+    "msg-checks",
+    "repstorm",
+    "groupmanager",
+    "sudo",
+    "phony",
+    "tools"
+  },
+  info_text = ":small_blue_diamond: Å\154á\142§Ã\155â\132\156Ï¾Ã\139Å\154 Å\154Í²á\142§â\132\156â±® v 1  :small_orange_diamond:\n\n\n:link: | [https://telegram.me/TEAMSTORM]\n\n\nð\159\148\151 | [ https://github.com/NOVAR1/STORM_cli_api.git]\n\n\nð\159\148\151 | Ä\144â\130¬V Å\154á\142§Ã\155â\132\156Ï¾Ã\139S\n\n\n:link: | Ä\144Ã\139V : @TAHAJ20\n\n\n:link: | Ä\144Ã\139V : @N0VAR\n\n\n :link: | Ã\135H BÃ\152T : @TAEMSTORM\n\n\n :link: | Ä\144Ã\134 : @TAHAJ20_BOT\n\n\n :link: | GRÃ\152P : https://t.me/joinchat/FklctEH6wIF8sFUZFMgEaQ\n\n\n\n Ø§ØªÙ\133Ù\134Ø§ Ø§Ù\132ØªÙ\136Ù\129Ù\138Ù\130 Ù\132Ù\132Ø¬Ù\133Ù\138Ø¹ :green_heart:\n\n.\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    373906612,
+    321050917,
+    380151571,
+    342647859
+  }
+}
+return _
+end

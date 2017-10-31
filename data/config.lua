edc9ef1f808d13c6d88369abc245f8fbdbda7f7e
zuaus <30482065+zuaus@users.noreply.github.com>
2017-10-31 17:50:09 +0300

    Update config.lua

--- data/config.lua
+++ data/config.lua
@@ -21,7 +21,6 @@ do local _ = {
     "repstorm",
     "groupmanager",
     "sudo",
-    "phony",
     "tools"
   },
   info_text = "THE SOREC STORM IS ONLI A GROPS 30K \n\n\n⛥| [https://github.com/zuaus/DEVSTORM.git]\n\n\n⛥| Đ€V ŚᎧÛℜϾËS\n\n\n⛥| ĐËV : @TAHAJ20\n\n\n⛥| ĐËV : @TAHAJ20_BOT\n\n\n⛥| ÇH BØT : @TEAMSTORM\n\n\n⛥| ĐÆ : @TAHAJ20_BOT\n\n\n⛥| GRØP : https://t.me/joinchat/FklctEH6wIF8sFUZFMgEaQ\n\n\n\n اتمنا التوفيق للجميع \n\n`⛥ ~~ ~~ ~~ ~~ ~~ ⛥`\n",

4a2012543b9a46c365481f0a2ddea7dbbff89181
zuaus <30482065+zuaus@users.noreply.github.com>
2017-11-03 21:38:30 +0300

    Update sudo.lua

--- plugins/sudo.lua
+++ plugins/sudo.lua
@@ -1,23 +1,31 @@
---خلي معرفاتك بين الاقواس واحفض الملف 
-local function run (msg , matches)
-if matches[1]:lower() == "المطور" or matches[1] == 'مطور' and is_sudo and owner and admin(msg) then
-if not lang then
-text = [[
+--Start 
+local function run (msg,matches)
 
-
- ]]
+  if matches[1]:lower() == 'تنظيف' and matches[2]:lower() == 'الحظر' then
+if not is_sudo(msg) then
+      return -- «Mods allowed»
+    end
+	    local function cleanbl(ext, res)
+      if tonumber(res.total_count_) == 0 then -- «Blocklist is empty or maybe Bot is not group's admin»
+        return tdcli.sendMessage(ext.chat_id, ext.msg_id, 0, '⛥┇قائمه الحظر خاليه من الاعضاء', 1 , 'md')
+      end
+      local x = 0
+      for x,y in pairs(res.members_) do
+        x = x + 1
+        tdcli.changeChatMemberStatus(ext.chat_id, y.user_id_, 'Left', dl_cb, nil) -- «Changing user status to left, removes user from blocklist»
+      end
+      return tdcli.sendMessage(ext.chat_id, ext.msg_id, 0, '⛥┇تم رفع الحظر عن جميع الاعضاء ✔', 1 , 'md')
+    end
+	
+    return tdcli.getChannelMembers(msg.to.id, 0, 'Kicked', 200, cleanbl, {chat_id = msg.to.id, msg_id = msg.id}) -- «Gets channel blocklist»
+  end
 
 end
-return text
-end
-end
-
 
 return {
-patterns ={
-"^(المطور)$",
- "^(مطور)$",
-}, 
-run =run
+  patterns ={
+    "^(تنظيف) (الحظر)$",
+  },
+  run = run,
 }
----امسح ملف الجهة الاتصال
+

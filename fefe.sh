4be1bb66e610397821ce0c0117759e33ece2c3e5
zuaus <30482065+zuaus@users.noreply.github.com>
2017-11-26 21:24:53 +0300

    Add files via upload

---
+++ fefe.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+ COUNTER=0
+while [  $COUNTER -lt 5 ]; do
+kill $(pgrep telegram-cli)
+echo -e "\e[38;5;77m"   
+echo -e "        \e[38;5;40m"
+sleep 2
+   ./tg/tgcli -s ./taha.lua
+sleep 3
+done

de92cbc4f75ecb7ef242608648fdd864991af0fc
zuaus <30482065+zuaus@users.noreply.github.com>
2017-12-02 22:05:14 +0300

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,38 @@
+do local _ = {
+  admins = {
+    {},
+    {},
+    {
+    },
+    {
+    }
+  },
+  disabled_channels = {},
+  enabled_plugins = {
+    "plog",
+    "addreply",
+    "msg-checks",
+    "repstorm",
+    "sudo",
+    "addgruop",
+    "kickdel",
+    "lockselph",
+    "siteengbot",
+    "info",
+    "tools",
+    "id",
+    "groupmanager",
+    "mee",
+    "banhammer",
+    "help"
+  },
+  info_text = "THE SOREC STORM IS ONLI A GROPS 30K \n\n\n📍 اهــلا بك عــزيزي\nٴ♦️•••••••••••⛥❉⛥•••••••••••♦️\n\n⛥┋ＤＥＶ=➤ @TAHAJ20\n\n⛥┋ＤＥＶ=➤ @DEV_STORM \n\n⛥┋ＣＨ =➤ @TEAMSTORM \n\n⛥┋Đ Æ =➤ @TAHAJ20_BOT \n\n⛥┋https://github.com/zuaus/DEVSTORM \n\nٴ♦️•••••••••••⛥❉⛥•••••••••••♦️\n\nبل توفيق للجميع انشالله 👲",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    373906612
+  }
+}
+return _
+end

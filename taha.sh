5a71b649fab02a8f7ae1dff501c4e3e8cc14814d
zuaus <30482065+zuaus@users.noreply.github.com>
2017-12-05 19:05:50 +0300

    Update taha.sh

--- taha.sh
+++ taha.sh
@@ -1,104 +1,22 @@
-#!/usr/bin/env bash
-
-cd $HOME/DEVSTORM
-token_="472266761:AAFbpl-MPVcZEgKnyPPh2qjhKK6P5AHVwZQ" 
-install() {
-	    cd tg
-		sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-		sudo apt-get install g++-4.7 -y c++-4.7 -y
-		sudo apt-get update
-		sudo apt-get upgrade
-		sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
-		sudo apt-get install screen -y
-		sudo apt-get install tmux -y
-		sudo apt-get install libstdc++6 -y
-		sudo apt-get install lua-lgi -y
-		sudo apt-get install libnotify-dev -y
-		sudo service redis-server restart
-		wget https://valtman.name/files/telegram-cli-1222
-		mv telegram-cli-1222 tgcli
-		chmod +x tgcli
-		cd ..
-  chmod +x tg
-  lua ./bot/bot.lua
-  chmod +x STORM.sh
-  ./STORM.sh
-}
-
-function print_logo() {
-	echo -e "
- __   ___________   ___     _____     __  __
-/  |  |___   ___|  / _ \   | ___ }   |  \/  |
-\_ \      | |     | | | |  | |_) }   | |\/| |
- _) |     | |     | |_| |  |  _< \   | |  | |
-|__/      |_|      \___/   |_|  \_\  |_|  |_|
- BY @TAHAJ20 DEV : TAHAJ20            "
-	echo -e " "
-	echo -e " "
-	echo -e " "
-	echo -e " "
-	echo -e " "
-}
-
-function logo_play() {
-    declare -A txtlogo
-    seconds=" "
-    txtlogo[1]=" "
-    txtlogo[2]=" "
-    txtlogo[3]=" "
-    txtlogo[4]=" "
-    printf "\033[38;5;600m\t"
-    for i in ${!txtlogo[@]}; do
-        for x in `seq 0 ${#txtlogo[$i]}`; do
-            printf "${txtlogo[$i]:$x:1}"
-            sleep $seconds
-        done
-        printf "\n\t"
-    done
-    printf "\n"
-}
-
-if [ "$1" = "install" ]; then
-  install
-  else
-if [ ! $token_ ]; then
-echo -e ""
-
-echo -e ""
-
-  echo -e "\e[31;1mToken Not found\e"
-
- exit 1
-
- fi
+token="472266761:AAFbpl-MPVcZEgKnyPPh2qjhKK6P5AHVwZQ"
 if [ ! -f ./tg/tgcli ]; then
     echo "tg not found"
     echo "Run $0 install"
     exit 1
- fi
-
-   print_logo
-   echo -e "\033[38;5;208m"
-   echo -e "
- __   ___________   ___     _____     __  __
-/  |  |___   ___|  / _ \   | ___ }   |  \/  |
-\_ \      | |     | | | |  | |_) }   | |\/| |
- _) |     | |     | |_| |  |  _< \   | |  | |
-|__/      |_|      \___/   |_|  \_\  |_|  |_|
- BY @TAHAJ20 DEV : TAHAJ20            
-"
-   echo -e "    "
-   echo -e "   "
-   echo -e "   "
-   echo -e "     "
-   echo -e "\033[0;00m"
-   echo -e "\e[36m"
-   logo_play
-   #sudo service redis-server restart
-   #./tg/tgcli -s ./bot/bot.lua -l 1 -E $@ --bot=$token_
-curl "https://api.telegram.org/bot"$token_"/sendmessage" -F
-
-./tg/tgcli -s ./bot/bot.lua $@ --bot=$token_
-
 fi
+if [ $token == "" ]; then
+    echo "token not found"
+    echo "Run install again"
+    exit 1
+fi
+ 
+COUNTER=1
+while(true) do
+
+curl "https://api.telegram.org/bot"$token"/sendmessage" -F
+./tg/tgcli -s ./bot/bot.lua $@ --bot=$token
+
+let COUNTER=COUNTER+1 
+done
+
 

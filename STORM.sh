a4bfe2edbf56675e0fb7c5fae64c43caaeb3a6c4
zuaus <30482065+zuaus@users.noreply.github.com>
2018-01-05 17:06:25 +0300

    Update STORM.sh

--- STORM.sh
+++ STORM.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd $HOME/DEVSTORM
-token_="475702712:AAGRxNsme_--3e6F1pHV8L9Tv1ALau4yD04" 
+token_="385190877:AAFkO796geK3YzM1KqjhtH7tALUPjgeQhMA" 
 install() {
 	    cd tg
 		sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
